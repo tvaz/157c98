fc5be6eaadec52016159153b67cd17728716a47a
tvaz <torivaz@protonmail.com>
2021-12-10 17:02:26 -0800

    Changes ADD_CONVERSATION and SET_NEW_MESSAGE actions

--- client/src/store/utils/reducerFunctions.js
+++ client/src/store/utils/reducerFunctions.js
@@ -1,21 +1,15 @@
 export const addMessageToStore = (state, payload) => {
   const { message, sender } = payload;
-  // if sender isn't null, that means the message needs to be put in a brand new convo
-  if (sender !== null) {
-    const newConvo = {
-      id: message.conversationId,
-      otherUser: sender,
-      messages: [message],
-    };
-    newConvo.latestMessageText = message.text;
-    return [newConvo, ...state];
-  }
 
   return state.map((convo) => {
     if (convo.id === message.conversationId) {
-      convo.messages.push(message);
-      convo.latestMessageText = message.text;
-      return convo;
+      const newConvo = {
+        ...convo,
+        id: message.conversationId,
+        messages: [message, ...convo.messages],
+        latestMessageText: message.text
+      }
+      return newConvo;
     } else {
       return convo;
     }
@@ -67,14 +61,17 @@ export const addSearchedUsersToStore = (state, users) => {
 };
 
 export const addNewConvoToStore = (state, recipientId, message) => {
-  return state.map((convo) => {
-    if (convo.otherUser.id === recipientId) {
-      convo.id = message.conversationId;
-      convo.messages.push(message);
-      convo.latestMessageText = message.text;
-      return convo;
-    } else {
-      return convo;
-    }
-  });
+   return state.map((convo) => {
+     if(convo.otherUser.id === recipientId) {
+         const newConvo = {
+            ...convo,
+           id: message.conversationId,
+           messages: [message],
+           latestMessageText: message.text
+         }
+         return newConvo;
+         } else {
+         return convo;
+        }
+   });
 };
