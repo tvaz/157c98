98ba9091446cf11c802057ee3c8e5b34b4b95d00
tvaz <torivaz@protonmail.com>
2021-12-23 17:44:33 -0800

    UI - Makes the preview text bolded in sidebar

--- client/src/components/Sidebar/ChatContent.js
+++ client/src/components/Sidebar/ChatContent.js
@@ -18,13 +18,21 @@ const useStyles = makeStyles((theme) => ({
     color: "#9CADC8",
     letterSpacing: -0.17,
   },
+  unreadPreviewText: {
+    fontWeight: "bold"
+  }
 }));
 
 const ChatContent = (props) => {
   const classes = useStyles();
 
   const { conversation } = props;
-  const { latestMessageText, otherUser } = conversation;
+  const { latestMessageText, latestSender, otherUser, unread } = conversation;
+
+  var showUnread = unread > 0;
+  if (latestSender != otherUser.id) {
+    showUnread = false;
+  }
 
   return (
     <Box className={classes.root}>
@@ -32,7 +40,7 @@ const ChatContent = (props) => {
         <Typography className={classes.username}>
           {otherUser.username}
         </Typography>
-        <Typography className={classes.previewText}>
+        <Typography className={classes.previewText, showUnread ? classes.unreadPreviewText : ""}>
           {latestMessageText}
         </Typography>
       </Box>
